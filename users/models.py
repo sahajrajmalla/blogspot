81190f41de716d88cf634e39e1e375722ba0da06
Sahaj Raj Malla <mallasahajraj@gmail.com>
2019-07-06 00:02:20 +0545

    added the update profile

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -11,3 +12,14 @@ class profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s profile picture"
+
+
+def save(self):
+    Super().save()
+
+    img = Image.open(self.image.path)
+
+    if img.height > 300 or img.width > 300:
+        output_size = (300, 300)
+        img.thumbnail(output_size)
+        img.save(self.p_picture.path)
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 # Create your views here.
 
 
@@ -22,4 +22,24 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == "POST":
+        Uform = UserUpdateForm(request.POST, instance=request.user)
+        Pform = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if Uform.is_valid() and Pform.is_valid():
+            Uform.save()
+            Pform.save()
+            username = Uform.cleaned_data.get('username')
+            messages.success(
+                request, f'Your account has been successfully updated', {username})
+            return redirect('profile')
+    else:
+        Uform = UserUpdateForm(instance=request.user)
+        Pform = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        "Uform": Uform,
+        "Pform": Pform,
+    }
+
+    return render(request, 'users/profile.html', context)
