1a11fd4851f40745e7e37518038e006783e0417f
Sahaj Raj Malla <mallasahajraj@gmail.com>
2019-07-09 03:07:17 +0545

    password reset functionality added excluding email service

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Blog
-from django.views.generic import ListView, DetailView
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
 
@@ -14,3 +14,47 @@ class BlogListView(ListView):
 
 class BlogDetailView(DetailView):
     model = Blog
+
+
+class BlogCreateView(LoginRequiredMixin, CreateView):
+    model = Blog
+    fields = ["title", "content"]
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class BlogUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+
+    model = Blog
+    fields = ["title", "content"]
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        blog = self.get_object()
+        if self.request.user == blog.author:
+            return True
+        else:
+            return False
+
+
+class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Blog
+    success_url = "/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        blog = self.get_object()
+        if self.request.user == blog.author:
+            return True
+        else:
+            return False
